7eb9022a0518c58d6bdd5d1072fb6622f3c902c2
Sahil Hingorani <sahilhingorani25@gmail.com>
2018-02-18 16:58:20 -0800

    dynamic post population to home page

--- homePage/js/main.js
+++ homePage/js/main.js
@@ -5,19 +5,84 @@ var titlePost = document.getElementById('titlePost');
 var itemDescription = document.getElementById('itemDescription');
 
 
-var playersRef = firebase.database().ref("/posts");
+// var playersRef = firebase.database().ref("/posts");
 
-playersRef.on("child_added", function(data, prevChildKey) {
-   var user = data.val();
-   console.log("title: " + user.titlePost);
-   console.log("description: " + user.itemDescription);
+// playersRef.on("child_added", function(data, prevChildKey) {
+//    var user = data.val();
+//    console.log("title: " + user.titlePost);
+//    console.log("description: " + user.itemDescription);
 
-   var titlePost = document.getElementById('titlePost').innerHTML =
-   user.titlePost;
+   
 
-   var itemDescription = document.getElementById('itemDescription').innerHTML =
-   user.itemDescription;
+// });
 
+
+function snapshotToArray(snapshot) {
+    var returnArr = [];
+
+    snapshot.forEach(function(childSnapshot) {
+        var item = childSnapshot.val();
+        item.key = childSnapshot.key;
+
+        returnArr.push(item);
+    });
+
+    return returnArr;
+}
+
+
+firebase.database().ref('/posts').on('value', function(snapshot) {
+    var arr = snapshotToArray(snapshot);
+    console.log(arr);
+
+    var rentList = document.getElementById('rentList');
+    rentList.innerHTML = '';
+
+    for(var i = 0; i < arr.length; i++) {
+       var title = arr[i].titlePost;
+	   var desc = arr[i].itemDescription;
+	   var img = arr[i].imageURL;
+	   var price = arr[i].pricePerHour;
+
+
+	   rentList.innerHTML += 
+						   '<div class="col-md-offset-2">' +
+						                '<div class="col-12 col-lg-4">' +
+						                    '<div class="card features">' +
+						                        '<div class="card-body">' +
+						                            '<div class="media">' +
+														'<img class="rentimg" id="image" src=' + img + 'class="listpics" alt="">' +
+						                                '<div class="media-body">' +
+						                                    '<h4 class="card-title" id="titlePost">' + title + '</h4>' +
+															'<h5 id="price" class = "card-price">$' + price + '/hr</h5>' +
+						                                    '<p class="card-text" id="itemDescription">' + desc + '</p>' +
+						                                '</div>' +
+						                            '</div>' +
+						                        '</div>' +
+						                    '</div>' +
+						                '</div>' +
+						            '</div>';
+
+    }
 });
+
+
+
+
+
+
+
+
+
+
+// var titlePost = document.getElementById('titlePost').innerHTML =
+//    user.titlePost;
+
+//    var itemDescription = document.getElementById('itemDescription').innerHTML =
+//    user.itemDescription;
+   
+//    document.getElementById('image').src = user.imageURL;
+
+//    document.getElementById('price').innerHTML = "$" + user.pricePerHour + "/hour";
 
 
