341227a3412398f1de79f4a7519870eebaeed5f5
Terrell Marshall <tmarshall@umail.ucsb.edu>
2018-02-18 18:37:58 -0800

    home-page and item-page javascript linking

---
+++ homePage/js/homePage-itemPage-main.js
@@ -0,0 +1,16 @@
+ // Initialize Firebase
+'use-strict';
+
+
+
+/*alert('THIS IS A FUCKING ALERT');*/
+
+
+
+	document.getElementById('anchor').addEventListener("click", sendInstancePID() );
+ 
+
+
+	function sendInstancePID() {
+		alert('I HAVE BEEN CALLED');
+	}
